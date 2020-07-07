910aaa973de4a98f0bceb09afb1f92c0a0017bc6
DevelopingCoffee <44442845+DevelopingCoffee@users.noreply.github.com>
2020-07-07 13:54:03 +0200

    Create is_nummer_palindrome.py

---
+++ is_nummer_palindrome.py
@@ -0,0 +1,15 @@
+def is_palindrome(num):
+    # Skip single-digit inputs
+    if num // 10 == 0:
+        return False
+    temp = num
+    reversed_num = 0
+
+    while temp != 0:
+        reversed_num = (reversed_num * 10) + (temp % 10)
+        temp = temp // 10
+
+    if num == reversed_num:
+        return num
+    else:
+        return False
