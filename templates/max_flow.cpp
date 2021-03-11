9fcbf2550eadacccec2423726c0b300d75612209
Dirk Neuhäuser <dirk.neu3@gmail.com>
2021-03-11 16:28:46 +0100

    added maxFlow/minCut algorithmn

---
+++ templates/max_flow.cpp
@@ -0,0 +1,72 @@
+// Calculates maxFlow == minCut in O(V^2 E)
+class MaxFlow {
+private:
+    int n;
+    vector<tuple<int, long long, long long>> edges; // (target node, capacity, flow used)
+    vector<vector<int>> graph;
+    vector<int> distance, last;
+    bool bfs(int s, int t){ // construct level graph and check if more flow is possible
+        distance.assign(n, -1);
+        distance[s]=0;
+        queue<int> q({s});
+        while (!q.empty()){
+            int cur = q.front(); q.pop();
+            if(cur == t){break;}
+            for(int idx: graph[cur]){
+                auto &[v, cap, flow] = edges[idx];
+                if((cap - flow) > 0 && distance[v]==-1){
+                    q.push(v);
+                    distance[v] = distance[cur] + 1;
+                }
+            }
+
+        }
+        return distance[t] != -1;
+
+    }
+
+    long long dfs(int cur, int t, long long f=1e18){ // send multiple flows from s to t through level graph
+        if(cur==t || f==0) return f;
+        for(int &i = last[cur]; i<(int)graph[cur].size(); ++i){
+            auto &[v, cap, flow] = edges[graph[cur][i]];
+            if(distance[cur] != distance[v] -1) continue;
+            if(long long pushed = dfs(v, t, min(f, cap-flow))){
+                flow += pushed;
+                long long &rflow = get<2>(edges[graph[cur][i]^1]); // clever way to get back edge number^1 -> number +1 if number even else number -1
+                rflow -= pushed;
+                return pushed;
+            }
+        }
+        return 0;
+    }
+public:
+    MaxFlow(int nodes): n(nodes) {
+        edges.clear();
+        graph.assign(n, vector<int>());
+    }
+    void add_edge(int cur, int v, long long w, bool directed=true){
+        if(cur == v){return;}
+        edges.emplace_back(v, w, 0);
+        graph[cur].push_back(edges.size()-1);
+        edges.emplace_back(cur, directed?0:w, 0);
+        graph[v].push_back(edges.size()-1);
+    }
+   long long dinic(int s, int t){ // calculates the maxFlow from s to t
+        long long maxFlow = 0;
+        while(bfs(s, t)){
+            last.assign(n, 0); // this remembers the index for the dfs
+            while(long long flow = dfs(s, t)){
+                maxFlow += flow;
+            }
+        }
+        return maxFlow;
+
+    }
+};
+
+// MaxFlow maxFlow = MaxFlow(4);
+// maxFlow.add_edge(0,1,10);
+// maxFlow.add_edge(1,2,5);
+// maxFlow.add_edge(2,3,10);
+// long long test = maxFlow.dinic(0, 3);
+// cout << test << endl;
